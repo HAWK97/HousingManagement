e3a466b4142786389d2fc0af940bfb6cd9ac9f4a
Hawk97 <wsghawk97@163.com>
2018-12-26 16:08:38 +0800

    搭建项目基本框架

---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dell
+  Date: 2018/12/26
+  Time: 15:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>房屋交易管理系统</title>
+</head>
+<body>
+哈哈哈，这里是房屋交易管理系统
+</body>
+</html>
---
+++ src/test/java/com/hawkbear/housingmanagement/HousingManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hawkbear.housingmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HousingManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
